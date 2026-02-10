856e01542d51aada5ad2c72f52ba9735bee64bb1
EliphazBouuye <eliphazbouye@gmail.com>
2026-02-10 00:02:29 +0000

    first commit

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,44 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.softtabstop = 2
+
+opt.smartindent = true
+opt.autoindent = true
+opt.wrap = false
+
+opt.hlsearch = true
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+opt.signcolumn = "yes"
+opt.cursorline = true
+opt.colorcolumn = "120"
+
+opt.clipboard = "unnamedplus"
+opt.mouse = "a"
+
+opt.splitbelow = true
+opt.splitright = true
+
+opt.swapfile = false
+opt.backup = false
+opt.undofile = true
+
+opt.updatetime = 250
+opt.timeoutlen = 300
+
+opt.termguicolors = true
+
+opt.list = true
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+opt.completeopt = "menuone,noselect"
